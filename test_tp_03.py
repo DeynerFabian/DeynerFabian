f87b15e1dfcb3faff93b5b46afe9f6ac60887fe0
DeynerFabian <deynerfserrano@gmail.com>
2022-11-02 19:45:42 -0300

    Agregando test

--- test_tp_03.py
+++ test_tp_03.py
@@ -1,10 +1,13 @@
 from posixpath import supports_unicode_filenames
-from tp_3 import *
+from tp_03 import *
 import pytest
 
 # utilidades
 sucursal_retiro = SucursalFisica()
-remera_talle_s= Prenda(100,"remera talle s",1500,"remera")
+remera_talle_s = Prenda(100,"remera talle s",1500,"remera")
+jean_talle_40 = Prenda(200, "jean_talle_40", 3000, "jean")
+gorra_blanca = Prenda(300, "gorra_blanca", 4500, "gorra")
+
 def reiniciar_listas(sucursal):
     sucursal.productos.clear()
     sucursal.ventas.clear()
@@ -29,3 +32,59 @@ def test_hay_stock():
     sucursal_retiro.recargar_stock(100, 500)
     assert sucursal_retiro.hay_stock(100) == True
     
+def test_calcular_precio_final_extranjero():
+    reiniciar_listas(sucursal_retiro)
+    sucursal_retiro.registrar_producto(remera_talle_s)
+    sucursal_retiro.registrar_producto(jean_talle_40)
+    sucursal_retiro.recargar_stock(100, 500)
+    sucursal_retiro.recargar_stock(200, 500)
+    assert sucursal_retiro.calcular_precio_final(remera_talle_s, True) == 1500
+
+def test_calcular_precio_final_local():
+    reiniciar_listas(sucursal_retiro)
+    sucursal_retiro.registrar_producto(remera_talle_s)
+    sucursal_retiro.registrar_producto(jean_talle_40)
+    sucursal_retiro.recargar_stock(100, 500)
+    sucursal_retiro.recargar_stock(200, 500)
+    assert sucursal_retiro.calcular_precio_final(jean_talle_40, False) == 3630
+
+def test_contar_por_categorias_varias():
+    reiniciar_listas(sucursal_retiro)
+    sucursal_retiro.registrar_producto(remera_talle_s)
+    sucursal_retiro.registrar_producto(jean_talle_40)
+    sucursal_retiro.recargar_stock(100, 500)
+    sucursal_retiro.recargar_stock(200, 500)
+    assert sucursal_retiro.contar_categorias() == 2
+
+def test_contar_por_categoria():
+    reiniciar_listas(sucursal_retiro)
+    sucursal_retiro.registrar_producto(remera_talle_s)
+    sucursal_retiro.registrar_producto(jean_talle_40)
+    sucursal_retiro.recargar_stock(100, 500)
+    assert sucursal_retiro.contar_categorias() == 2
+
+def test_realizar_compra():
+    reiniciar_listas(sucursal_retiro)
+    sucursal_retiro.registrar_producto(remera_talle_s)
+    sucursal_retiro.registrar_producto(jean_talle_40)
+    sucursal_retiro.recargar_stock(100, 500)
+    sucursal_retiro.recargar_stock(200, 500)
+    sucursal_retiro.realizar_compra(100,1, True)
+    assert len(sucursal_retiro.ventas) == 1
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+    
+
