950959cc538cbe18456a78b4a6919f65d1bcb887
DeynerFabian <deynerfserrano@gmail.com>
2022-11-03 12:17:53 -0300

    Agregando mas tests

--- test_tp_03.py
+++ test_tp_03.py
@@ -73,7 +73,45 @@ def test_realizar_compra():
     assert len(sucursal_retiro.ventas) == 1
 
 
+def test_calcular_precio_final_extranjero():
+    reiniciar_listas(sucursal_retiro)
+    sucursal_retiro.registrar_producto(remera_talle_s)
+    sucursal_retiro.registrar_producto(jean_talle_40)
+    sucursal_retiro.recargar_stock(100, 500)
+    sucursal_retiro.recargar_stock(200, 500)
+    assert sucursal_retiro.calcular_precio_final(remera_talle_s, True) == 1500
 
+def test_calcular_precio_final_local():
+    reiniciar_listas(sucursal_retiro)
+    sucursal_retiro.registrar_producto(remera_talle_s)
+    sucursal_retiro.registrar_producto(jean_talle_40)
+    sucursal_retiro.recargar_stock(100, 500)
+    sucursal_retiro.recargar_stock(200, 500)
+    assert sucursal_retiro.calcular_precio_final(jean_talle_40, False) == 3630
+
+def test_contar_por_categorias_varias():
+    reiniciar_listas(sucursal_retiro)
+    sucursal_retiro.registrar_producto(remera_talle_s)
+    sucursal_retiro.registrar_producto(jean_talle_40)
+    sucursal_retiro.recargar_stock(100, 500)
+    sucursal_retiro.recargar_stock(200, 500)
+    assert sucursal_retiro.contar_categorias() == 2
+
+def test_contar_por_categoria():
+    reiniciar_listas(sucursal_retiro)
+    sucursal_retiro.registrar_producto(remera_talle_s)
+    sucursal_retiro.registrar_producto(jean_talle_40)
+    sucursal_retiro.recargar_stock(100, 500)
+    assert sucursal_retiro.contar_categorias() == 2
+
+def test_realizar_compra():
+    reiniciar_listas(sucursal_retiro)
+    sucursal_retiro.registrar_producto(remera_talle_s)
+    sucursal_retiro.registrar_producto(jean_talle_40)
+    sucursal_retiro.recargar_stock(100, 500)
+    sucursal_retiro.recargar_stock(200, 500)
+    sucursal_retiro.realizar_compra(100,1, True)
+    assert len(sucursal_retiro.ventas) == 1
     
     
 
